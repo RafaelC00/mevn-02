135ace66cf4c74215cf510a2ce19c2b78635c949
RafaelC00 <rafacaicedo@hotmail.com>
2022-10-08 22:26:23 -0300

    port listeting to env

--- api/src/app.ts
+++ api/src/app.ts
@@ -11,6 +11,6 @@ app.get('/', (req , res) => {
   res.send('Hello World!')
 } )
 
-app.listen(port, () => {
+app.listen(`0.0.0.0:${port}`, () => {
   console.log(`Example app listening on port ${port}`)
 } )
